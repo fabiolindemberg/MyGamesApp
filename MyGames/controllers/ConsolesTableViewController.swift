033189b6266ca78cdb663fd1c8e48a62891fb82b
Fábio Lindemberg <fabiolindemberg@gmail.com>
2019-11-30 14:07:48 -0300

    Initial commit

---
+++ MyGames/controllers/ConsolesTableViewController.swift
@@ -0,0 +1,142 @@
+//
+//  ConsolesTableViewController.swift
+//  MyGames
+//
+//  Created by Fabio Estudo on 29/11/19.
+//  Copyright © 2019 Fabio Estudo. All rights reserved.
+//
+
+import UIKit
+
+class ConsolesTableViewController: UITableViewController {
+
+    let consolesManager = ConsolesManager.shared
+
+    fileprivate func loadConsoles() {
+        consolesManager.loadConsoles(with: context)
+        tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpStatusBarBackgroudColorWhenIOS13(color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
+        loadConsoles()
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return consolesManager.consoles.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let console = consolesManager.consoles[indexPath.row]
+        cell.textLabel?.text = console.name
+        return cell
+    }
+    
+    private func showAlert(with console: Console?) {
+        let title = console == nil ? "Adicionar" : "Editar"
+        let alert = UIAlertController(title: title + " plataforma", message: nil, preferredStyle: .alert)
+       
+        alert.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "Nome da plataforma"
+           
+            if let name = console?.name {
+                textField.text = name
+            }
+        })
+       
+        alert.addAction(UIAlertAction(title: title, style: .default, handler: {(action) in
+            let console = console ?? Console(context: self.context)
+            console.name = alert.textFields?.first?.text
+            do {
+                try self.context.save()
+                self.loadConsoles()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }))
+       
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+        alert.view.tintColor = UIColor(named: "second")
+       
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func addConsole(_ sender: UIBarButtonItem) {
+        showAlert(with: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         let console = consolesManager.consoles[indexPath.row]
+         showAlert(with: console)
+        
+         // deselecionar atual cell
+         tableView.deselectRow(at: indexPath, animated: false)
+     }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+         if editingStyle == .delete {
+             consolesManager.deleteConsole(index: indexPath.row, context: context)
+             tableView.deleteRows(at: [indexPath], with: .fade)
+         }
+     }
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyGames/controllers/GamesTableViewController.swift
@@ -0,0 +1,169 @@
+//
+//  GamesTableViewController.swift
+//  MyGames
+//
+//  Created by Fabio Estudo on 29/11/19.
+//  Copyright © 2019 Fabio Estudo. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class GamesTableViewController: UITableViewController {
+
+    var fetchedResultController: NSFetchedResultsController<Game>!
+    let searchController = UISearchController(searchResultsController: nil)
+//    private func loadGames() {
+//        let fetchRequest: NSFetchRequest<Game> = Game.fetchRequest()
+//        let gameTitleSortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+//        fetchRequest.sortDescriptors = [gameTitleSortDescriptor]
+//        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+//        fetchedResultController.delegate = self
+//
+//        do {
+//             try fetchedResultController.performFetch()
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+//    }
+    
+    func loadGames(filtering: String = "") {
+        let fetchRequest: NSFetchRequest<Game> = Game.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+       
+        if !filtering.isEmpty {
+            // usando predicate: conjunto de regras para pesquisas
+            // contains [c] = search insensitive (nao considera letras identicas)
+            let predicate = NSPredicate(format: "title contains [c] %@", filtering)
+            fetchRequest.predicate = predicate
+        }
+       
+        // possui
+        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultController.delegate = self
+       
+        do {
+            try fetchedResultController.performFetch()
+        } catch  {
+            print(error.localizedDescription)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! GameViewController
+        vc.game = fetchedResultController.fetchedObjects?[tableView.indexPathForSelectedRow!.row]
+    }
+
+
+    fileprivate func setupSearchController() {
+        // altera comportamento default que adicionava background escuro sobre a view principal
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.tintColor = .white
+        searchController.searchBar.barTintColor = .white
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = true
+        
+        // usando extensions
+        searchController.searchBar.delegate = self
+        searchController.searchResultsUpdater = self
+
+        /*
+        FIX BUG black screen
+         Source: https://stackoverflow.com/questions/38836862/tab-bar-view-goes-blank-when-switched-back-to-with-search-bar-active
+         */
+        self.definesPresentationContext = true
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.definesPresentationContext = true
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.setUpStatusBarBackgroudColorWhenIOS13(color: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))
+        
+        setupSearchController()
+        
+        loadGames()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        let count = fetchedResultController?.fetchedObjects?.count ?? 0
+        
+        if count == 0 {
+            let label = UILabel()
+            label.text = "Nenhum jogo cadastrado!"
+            tableView.backgroundView = label
+        }
+        return count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! GameTableViewCell
+        guard let game = fetchedResultController.fetchedObjects?[indexPath.row] else {
+           return cell
+        }
+               
+        cell.prepare(with: game)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            guard let game = fetchedResultController.fetchedObjects?[indexPath.row] else {return}
+            context.delete(game)
+           
+            do {
+                try context.save()
+            } catch  {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension GamesTableViewController: NSFetchedResultsControllerDelegate {
+   
+    // sempre que algum objeto for modificado esse metodo sera notificado
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+       
+        switch type {
+            case .delete:
+                if let indexPath = indexPath {
+                    // Delete the row from the data source
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+                }
+                break
+            default:
+                tableView.reloadData()
+        }
+    }
+}
+
+extension GamesTableViewController: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        // TODO
+    }
+   
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        loadGames()
+        tableView.reloadData()
+       
+    }
+   
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        loadGames(filtering: searchBar.text!)
+        tableView.reloadData()
+    }
+}
