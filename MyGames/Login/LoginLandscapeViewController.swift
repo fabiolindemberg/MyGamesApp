15142ec760aefcf86c175d46737d9d6b951523cd
Fábio Lindemberg <fabiolindemberg@gmail.com>
2020-01-23 01:44:52 -0300

    Adicionando Login

---
+++ MyGames/Login/LoginLandscapeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginLandscapeViewController.swift
+//  MyGames
+//
+//  Created by Fabio Pereira on 23/01/20.
+//  Copyright © 2020 Fabio Estudo. All rights reserved.
+//
+
+import UIKit
+
+class LoginLandscapeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
